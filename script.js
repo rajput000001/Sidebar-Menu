d0c70c4bdbb9f7b84f564489b6fb431c1f98a07c
Saurabh Singh <129092699+rajput000001@users.noreply.github.com>
2024-03-05 22:11:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggler = document.querySelector('.toggler')
+const sidebar = document.querySelector('.sidebar')
+
+const showFull = () => {
+    toggler.addEventListener('click', ()=> {
+        toggler.classList.toggle('active')
+        sidebar.classList.toggle('active')
+    })
+}
+
+showFull()
